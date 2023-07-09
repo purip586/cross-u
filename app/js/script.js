930544d490aed695efbbf7e6c348c0dc9a63b868
puri <purip586@gmail.com>
2023-07-09 19:46:29 +0900

    ccc

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,32 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function(){
-  console.log('click hamburger');
-
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
+btnHamburger.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
     });
-    
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
+});
 
-  }  
+//cta
+const cta = document.getElementById("cta");
+
+cta.addEventListener("click", function () {
+  console.log("click start");
 });
