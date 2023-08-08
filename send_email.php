56896cb12d12ae273c21c60576492c71b3f1c6f1
puri <purip586@gmail.com>
2023-08-08 18:16:45 +0900

    ccc

--- send_email.php
+++ send_email.php
@@ -3,17 +3,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
     $name = $_POST["name"];
     $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    $location = $_POST["location"];
+    $visa = $_POST["visa"];
+    $income = $_POST["income"];
 
     $recipient = "puri@xu-1.co.jp";
 
     // Create email headers
     $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
+    $headers = "Reply-To: $email\r\n";
     
     // Send the email
-    if (mail($recipient, $subject, $message, $headers)) {
+    if (mail($recipient, $headers, $name, $email, $location, $visa, $income)) {
         echo "Thank you for contacting us! Your message has been sent successfully.";
     } else {
         echo "Oops! Something went wrong. Please try again later.";
